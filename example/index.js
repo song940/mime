09b01a3fe220a0dac34adcd52c67799b5cb4407d
Lsong <song940@gmail.com>
2018-10-22 15:35:24 +0800

    fix bug and publish to 0.0.6

--- example/index.js
+++ example/index.js
@@ -3,29 +3,8 @@ const Message = require('..');
 
 const message = new Message();
 
-message.on('end', (headers, body) => {
-  console.log(body);
+fs.createReadStream(__dirname + '/../docs/smtp-gmail.txt').pipe(message);
+
+message.on('end', () => {
+  console.log(message.headers);
 });
-
-fs.createReadStream(__dirname + '/../docs/smtp-qq.txt').pipe(message);
-
-// const type = new Message.Header({
-//   name: 'content-type',
-//   value: 'text/html',
-//   options: { a: 1, b:2 }
-// });
-
-// const html = new Message([
-//   type,
-// ], '<h1>Hello text/html</h1>');
-
-// const message = new Message({
-//   From: 'mail@lsong.org',
-//   To: 'hi@lsong.org',
-//   Subject: 'hello world',
-//   type
-// }, [
-//   html, 'hello text/plain!'
-// ]);
-
-// console.log(message.toString());
